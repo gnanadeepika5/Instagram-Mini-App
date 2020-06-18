cf36a017f8920e899de709dc0558ae3ef91d8c13
Meghamalak <meghamalakarnati26@icloud.com>
2020-06-17 17:29:52 -0700

    Added Profile, Passport and Gravatar

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
