909aed182b5becf050676b729b78ee1c1fa81969
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-06-19 01:47:20 -0700

    Passport authentification

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../../models/User');
 const keys = require('../../config/keys');
@@ -83,4 +84,15 @@ router.post('/login', (req, res) => {
     })
     .catch();
 })
+
+// @route   CET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', 
+passport.authenticate('jwt', {session:false}),
+(req,res) => {
+  return res.json({msg:'Success'});
+})
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
-// const passport = require('passport');
+const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
@@ -12,8 +12,8 @@ app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
 //Passport configuration
-//app.use(passport.initialize());
-// require('./config/passport')(passport);
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 //db configuration
 const db = require('./config/keys').mongoURI;
