df593db1ded763f80aeac185e0bc65cd25a343b9
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-06-19 15:17:19 -0700

    App POSTS->Basic post and get routes to & from DB

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,37 @@
 const express = require('express');
+const Post = require('../../models/Post');
 const router = express.Router();
-//test route from posts
-router.get('/test', (req,res) => res.json({msg:'posts worked'}));
+// //test route from posts
+// router.get('/Post', (req,res) => res.json({msg:'posts worked'}));
+
+
+// @route   POST api/users/AddPost
+// @desc    Add  new Post
+// @access  public 
+router.post('/newPost', (req, res) =>
+{
+  const newPost = new Post
+  ({
+    name: req.body.name,
+    email: req.body.email
+  });
+  console.log(`NewPost created. ${newPost.name}, ${newPost.email}`);
+  newPost.save()
+         .then(post => res.json(post))
+         .catch(err => console.log(err));
+
+})
+router.get('/Post',(req,res) =>{
+  Post.find({"email":"gnana5@gmail.com"})
+  .then((post) => res.json(post))
+  // .then(res.send({msg:'found' }))
+  .catch(err=>console.log(err));
+  
+  
+})
+
+
 module.exports = router;
+
+
+
