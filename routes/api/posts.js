ce4b560f7cef8830db99aa99a46ff348211fe856
Syam Kiran Dasari <Skirand@microsoft.com>
2020-06-20 17:38:34 -0700

    Fix

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,29 +7,29 @@ const router = express.Router();
 
 // @route   POST api/users/AddPost
 // @desc    Add  new Post
-// @access  public 
-router.post('/newPost', (req, res) =>
-{
-  const newPost = new Post
-  ({
-    name: req.body.name,
-    email: req.body.email
-  });
-  console.log(`NewPost created. ${newPost.name}, ${newPost.email}`);
-  newPost.save()
-         .then(post => res.json(post))
-         .catch(err => console.log(err));
+// // @access  public 
+// router.post('/newPost', (req, res) =>
+// {
+//   const newPost = new Post
+//   ({
+//     name: req.body.name,
+//     email: req.body.email
+//   });
+//   console.log(`NewPost created. ${newPost.name}, ${newPost.email}`);
+//   newPost.save()
+//          .then(post => res.json(post))
+//          .catch(err => console.log(err));
 
-})
-router.get('/Post',(req,res) => {
-  console.log("printing");
-  console.log(req.query);//in postman, give email:some id from db in params tab
-  const email = (req.query).email;
-  Post.find({email})
-  .then((post) => res.json(post))
-  // .then(res.send({msg:'found' }))
-  .catch(err=>console.log(err));
-})
+// })
+// router.get('/Post',(req,res) => {
+//   console.log("printing");
+//   console.log(req.query);//in postman, give email:some id from db in params tab
+//   const email = (req.query).email;
+//   Post.find({email})
+//   .then((post) => res.json(post))
+//   // .then(res.send({msg:'found' }))
+//   .catch(err=>console.log(err));
+// })
 
 
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,100 +7,99 @@ const router = express.Router();
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 
-// // @route   POST api/users/register
-// // @desc    Register user
-// // @access  Public 
-// router.post('/register', (req, res) => {
-// User.findOne({email: req.body.email})
-// .then(user => {
-//   if (user){
-//     return res.status(400).json({email: 'Email already exists'});
-//   } else {
-//     const avatar = gravatar.url(req.body.email, {
-//       s: '200',
-//       r: 'pg',
-//       d: 'mm'
-//     });
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public 
+router.post('/register', (req, res) => {
+User.findOne({email: req.body.email})
+.then(user => {
+  if (user){
+    return res.status(400).json({email: 'Email already exists'});
+  } else {
+    const avatar = gravatar.url(req.body.email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
 
-//     const newUser = new User({
-//       name: req.body.name,
-//       email: req.body.email,
-//       avatar,
-//       password: req.body.password
-//     });
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      avatar,
+      password: req.body.password
+    });
 
-//     bcrypt.genSalt(10, (err, salt) => {
-//       if (err) throw err;
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) throw err;
 
-//       bcrypt.hash(newUser.password, salt, (err, hash) => {
-//         if (err) throw err;
-//         newUser.password = hash;
-//         newUser
-//         .save()
-//         .then((user) => res.json(user))
-//         .catch((err) => console.log(err));
-//       });
-//     });
-//     }
-// })
-// .catch(err => console.log(err));
-// })
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+        .save()
+        .then((user) => res.json(user))
+        .catch((err) => console.log(err));
+      });
+    });
+    }
+})
+.catch(err => console.log(err));
+})
 
 
-// // @route   get api/users/search
-// // @desc    search user
-// // @access  Public 
-// router.get('/search/:userName', (req, res) => {
-//     const {userName} = req.params;
-//     return res.status(200).send(`{msg: searching user ${userName}}`);
-// })
+// @route   get api/users/search
+// @desc    search user
+// @access  Public 
+router.get('/search/:userName', (req, res) => {
+    const {userName} = req.params;
+    return res.status(200).send(`{msg: searching user ${userName}}`);
+})
 
-// // @route   get api/users/showPeople
-// // @desc    showPeople
-// // @access  Public 
-// router.get('/showPeople', (req, res) => {
-//     return res.status(200).send(`{msg: respond with all people info}`);
-// })
+// @route   get api/users/showPeople
+// @desc    showPeople
+// @access  Public 
+router.get('/showPeople', (req, res) => {
+    return res.status(200).send(`{msg: respond with all people info}`);
+})
 
-// // @route   get api/users/showMatches
-// // @desc    show Matches
-// // @access  Public 
-// router.get('/showMatches', (req, res) => {
+// @route   get api/users/showMatches
+// @desc    show Matches
+// @access  Public 
+router.get('/showMatches', (req, res) => {
   
-//   return res.status(200).send(`{msg: respond with matching people}`);
-// })
+  return res.status(200).send(`{msg: respond with matching people}`);
+})
 
-// // @route   post api/users/:userName
-// // @desc    post message to a user
-// // @access  Public 
-// router.post('/messages', (req, res) => {
-//   const toUser =  req.body.toUser;
-//   const msg =  req.body.msg;
-//   if(toUser=='')
-//     {
-//       return res.status(400).send ({msg: 'user is mandatory'});
-//     }
-//   if(msg =="" || msg === undefined)
-//     {
-//       return res.status(400).send({msg: 'blank messages can not be sent'});
-//     }
-//   return res.status(200).send(`{msg: sending message ${msg} to user ${toUser}}`);
-// })
+// @route   post api/users/:userName
+// @desc    post message to a user
+// @access  Public 
+router.post('/messages', (req, res) => {
+  const toUser =  req.body.toUser;
+  const msg =  req.body.msg;
+  if(toUser=='')
+    {
+      return res.status(400).send ({msg: 'user is mandatory'});
+    }
+  if(msg =="" || msg === undefined)
+    {
+      return res.status(400).send({msg: 'blank messages can not be sent'});
+    }
+  return res.status(200).send(`{msg: sending message ${msg} to user ${toUser}}`);
+})
 
-// // @route   get api/users/messages
-// // @desc    post message to a user
-// // @access  Public 
-// router.get('/messages', (req, res) => {
-//   return res.status(200).send(`{msg: get Messages from others}`);
+// @route   get api/users/messages
+// @desc    post message to a user
+// @access  Public 
+router.get('/messages', (req, res) => {
+  return res.status(200).send(`{msg: get Messages from others}`);
 
-// })
+})
 
 // @route   POST api/users/test
 // @desc    Register user
 // @access  Public 
 router.get('/test', (req, res) => {
     return res.status(200).send('{msg: user test}')
-
   });
 // @route   POST api/users/login
 // @desc    Login user/returning a token
