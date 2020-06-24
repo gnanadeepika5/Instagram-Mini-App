18bfca78dcdbf87074276c1b4e0510a925bd85b1
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-06-23 22:49:43 -0700

    Validation for register by email, required and invallid

--- validations/register.js
+++ validations/register.js
@@ -7,6 +7,15 @@ module.exports = function validateRegisterInput(data){
   if (!validator.isLength(data.name, {min: 2, max: 30} )){
     errors.name = 'Name must be between 2 and 30 characters';
   }
+
+  if (!validator.isEmail(data.email)){
+    errors.email = 'Email field is invalid';
+  }
+
+  if (isEmpty(data.email)){
+    errors.email = 'Email field is required';
+  }
+
 return {
   errors,
   isValid: isEmpty(errors)
