5ad38d7588a1d2439786538c3aff8ee79cda7fb4
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-07-12 20:05:11 -0700

    Added landing,footer,navbar with bg img bootstrap

--- instagram-mini-app-client/src/App.js
+++ instagram-mini-app-client/src/App.js
@@ -1,31 +1,21 @@
-import React from 'react';
-import video from './clouds-video.mp4';
-import image from './instagram-background-image.jpg';
-// import logo from './logo.svg';
+import React, {Component} from 'react';
+import BackgroundVideo from './components/BackgroundVideo/BackgroundVideo';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <video className='videoTag' autoPlay loop muted>
-         <source src={video} type='video/mp4' />
-      </video>
-      {/* <img src={image} className='image' alt='image' /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+         {/* <BackgroundVideo /> */}
+          <Navbar />
+          <Landing />
+          <Footer />
+        </div>
+    )
+  }
 }
 
 export default App;
---
+++ instagram-mini-app-client/src/components/Layout/Landing.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Instagram Mini
+            </h1>
+                <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+                <hr />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing;
