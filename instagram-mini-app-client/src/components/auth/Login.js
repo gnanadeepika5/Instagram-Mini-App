03a8c1b4de05a6801458b4190a03ab57134a6ab4
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-07-13 15:29:06 -0700

    bug fixes

--- instagram-mini-app-client/src/components/auth/Login.js
+++ instagram-mini-app-client/src/components/auth/Login.js
@@ -8,7 +8,7 @@ import React, { Component } from 'react'
       <div className="row">
         <div className="col-md-8 m-auto">
           <h1 className="display-4 text-center">Log In</h1>
-          <p className="lead text-center">Sign in to your Instagram account</p>
+          <p className="lead text-center">Sign in to your DevConnector account</p>
           <form action="dashboard.html">
             <div className="form-group">
               <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
