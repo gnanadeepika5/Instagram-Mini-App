3f6b3eb6409c890095ed92fbc29c339afce6316a
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-07-21 09:35:17 -0700

    Redux implementation for Login, added Logout functionality

--- instagram-mini-app-client/src/App.js
+++ instagram-mini-app-client/src/App.js
@@ -13,6 +13,30 @@ import Post from './components/posts/Post';
 //import LikesProfiles from './components/profiles/LikesProfiles';
 import './App.css';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { SET_CURRENT_USER } from './action/dispatchTypes';
+import { logoutUser } from './action/authActions';
+
+//Check for token
+if (localStorage.jwtToken){
+//Set auth header with token
+setAuthToken(localStorage.jwtToken);
+//decode token
+const decoded = jwt_decode(localStorage.jwtToken);
+//write user data to redux store
+store.dispatch({
+  type: SET_CURRENT_USER,
+  payload: decoded
+  });
+//check for expired token
+const currentTime = Date.now() / 1000;
+if (decoded.exp < currentTime) {
+  store.dispatch(logoutUser());
+
+  window.location.href='/login';
+}
+}
 
 class App extends Component {
   render() {
--- instagram-mini-app-client/src/components/Layout/Navbar.js
+++ instagram-mini-app-client/src/components/Layout/Navbar.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 //import logo from '../../img/instagram-logo.svg';
+import {connect} from 'react-redux';
+import {logoutUser} from '../../action/authActions';
 
 class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
-        <Link className="navbar-brand" to="/">
-        <img src="" width="30" height="30" className="d-inline-block align-top" alt=""/>
-          Instagram
-          </Link>
-         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+  onLogoutClick(e){
+    e.preventDefault();
+    this.props.logoutUser();
+  }
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/feed">
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a
+            href=""
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-link"
+          >
+            <img
+              className="rounded-circle"
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: "25px", marginRight: "5px" }}
+              title="You must have a gravatar connected to your email to display an image"
+            />
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+
+    return (
+      
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Instagram
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#mobile-nav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="mobile-nav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/profiles">
+                {" "}
+                Profiles
+              </Link>
+            </li>
+          </ul>
+
+         
+          <div className="collapse navbar-collapse" id="mobile-nav"></div>
             {/* <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <a className="nav-link" href="profiles.html"> 
@@ -23,26 +94,22 @@ class Navbar extends Component {
               </li>
             </ul> */}
             
-
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                <Link className="nav-link" to="/postForm"><i className="fas fa-plus"></i></Link>              
               </li>
-              <li className="nav-item">
-               <Link className="nav-link" to="/dashboard"><i className="fas fa-home"></i></Link>              
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/register">Sign Up</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-               </li>
-            </ul>
+            </ul> 
+          
+          {isAuthenticated ? authLinks : guestLinks}
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, {logoutUser})(Navbar);
